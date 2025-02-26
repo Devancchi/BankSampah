de2128b2de6ebf65c08a7085d397801e124cdd3c
devan <devan@10.10.181.95>
2025-02-26 15:13:34 +0700

    ubah tes

--- src/main/java/main/BankSampah.java
+++ src/main/java/main/BankSampah.java
@@ -11,6 +11,6 @@ package main;
 public class BankSampah {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("test change World!");
     }
 }
