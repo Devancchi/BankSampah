007a9d8f4e656f8640e055573648dc314533aa79
devan <devan@10.10.180.153>
2025-03-03 09:29:58 +0700

    add db connect

---
+++ src/main/java/main/DBconnect.java
@@ -0,0 +1,41 @@
+package main;
+
+/**
+ *
+ * @author devan
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnect {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/bank_sampah";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load MySQL JDBC Driver
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Koneksi Berhasil!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver tidak ditemukan: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Koneksi ke database gagal: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = getConnection();
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
