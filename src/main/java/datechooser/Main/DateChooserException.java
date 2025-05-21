92f3fb113501a1a143e99f11ec11f1dd15e93706
krizzn65 <krisnadewa6565@gmail.com>
2025-05-21 12:49:23 +0700

    revisi desain

---
+++ src/main/java/datechooser/Main/DateChooserException.java
@@ -0,0 +1,8 @@
+package datechooser.Main;
+
+public class DateChooserException extends RuntimeException {
+
+    public DateChooserException(String errorMessage) {
+        super(errorMessage);
+    }
+}
