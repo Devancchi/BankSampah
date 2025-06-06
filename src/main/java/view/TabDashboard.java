3ad0726f7e43821cb0667d51e31258b86a6c149a
33 <33@10.10.180.156>
2025-06-06 08:41:59 +0700

    update stok sampah/ manajemen sampah dan card dashboard

--- src/main/java/view/TabDashboard.java
+++ src/main/java/view/TabDashboard.java
@@ -683,7 +683,7 @@ public class TabDashboard extends javax.swing.JPanel {
             }
 
             // Total Sampah
-            ResultSet rsSampah = stmt.executeQuery("SELECT SUM(berat_sampah) FROM setor_sampah");
+            ResultSet rsSampah = stmt.executeQuery("SELECT SUM(stok_sampah) FROM sampah");
             if (rsSampah.next()) {
                 double totalSampah = rsSampah.getDouble(1);
                 lb_totalSampah.setText(String.valueOf(totalSampah) + "KG");
